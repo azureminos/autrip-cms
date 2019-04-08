a2136571d9058b7ce41cf1ae6a18440fbf776795
David Xia <apolo2david@yahoo.com>
2019-04-08 19:00:35 +1000

    Create FlightRate.js

---
+++ models/FlightRate.js
@@ -0,0 +1,36 @@
+var keystone = require('keystone');
+var Types = keystone.Field.Types;
+
+/**
+ * FlightRate Model
+ * ==========
+ */
+var FlightRate = new keystone.List('FlightRate', {
+  map: { name: 'name' },
+  singular: 'FlightRate',
+  plural: 'FlightRate',
+  autokey: { path: 'slug', from: 'name', unique: true },
+  track: true,
+});
+
+FlightRate.add({
+  package: { type: Types.Relationship, ref: 'Package' },
+  name: { type: Types.Text, required: true, index: true },
+  description: { type: Types.Textarea },
+  airline: { type: Types.Text },
+  type: { type: Types.Select, options: 'Economic, Economic Premium, Business', index: true },
+  rangeFrom: { type: Types.Datetime, default: Date.now },
+  rangeTo: { type: Types.Datetime, default: Date.now },
+  cost: { type: Types.Number, default: 0 },
+  rate: { type: Types.Number, default: 0 },
+  priority: { type: Types.Number, default: 0 },
+  notes: { type: Types.Textarea },
+  additionalField: { type: Types.Textarea },
+});
+
+FlightRate.defaultColumns = 'name, airline|15%, type|15%, rangeFrom|15%, rangeTo|15%, priority|15%';
+
+/**
+ * Registration
+ */
+FlightRate.register();
