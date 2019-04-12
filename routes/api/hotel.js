6a9297068a0df82e4e68616850a521cb620106d4
David Xia <apolo2david@yahoo.com>
2019-04-12 20:39:13 +1000

    Update hotel.js

--- routes/api/hotel.js
+++ routes/api/hotel.js
@@ -1,14 +1,15 @@
 // API to get Hotel
-var keystone = require('keystone'),
-    Hotel = keystone.list('Hotel');
+var keystone = require('keystone');
+var Hotel = keystone.list('Hotel');
+var City = keystone.list('City');
 
 /** * Get List of Hotel */
-exports.getHotel = function (req, res) {
+exports.getAllHotel = function (req, res) {
     Hotel.model.find().populate('city, nearByAttractions').exec(function (err, items) {
         if (err) return res.apiError('database error', err);
-        res.apiResponse(items);
+        return res.apiResponse(items);
     });
-}
+};
 
 /** * Get Hotel by ID */
 exports.getHotelById = function (req, res) {
@@ -17,4 +18,16 @@ exports.getHotelById = function (req, res) {
         if (!item) return res.apiError('not found');
         return res.apiResponse(item);
     });
-}
+};
+
+/** * Get Hotel by City */
+exports.getHotelByCity = function (req, res) {
+    var cityId = req.params.id;
+    City.model
+        .findById(cityId).populate('hotel')
+        .exec(function (err, item) {
+            if (err) return res.apiError('database error', err);
+            if (!item) return res.apiError('not found');
+            return res.apiResponse(item.hotel);
+        });
+};
