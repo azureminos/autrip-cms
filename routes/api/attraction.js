48f50a72360bf62f6b7cd4511513b3b32d311963
David Xia <apolo2david@yahoo.com>
2019-04-12 20:40:46 +1000

    Update attraction.js

--- routes/api/attraction.js
+++ routes/api/attraction.js
@@ -1,12 +1,13 @@
 // API to get Attraction
-var keystone = require('keystone'),
-    Attraction = keystone.list('Attraction');
+var keystone = require('keystone');
+var Attraction = keystone.list('Attraction');
+var City = keystone.list('City');
 
 /** * Get List of Attraction */
-exports.getAttraction = function (req, res) {
+exports.getAllAttraction = function (req, res) {
     Attraction.model.find().populate('city, nearByAttractions').exec(function (err, items) {
         if (err) return res.apiError('database error', err);
-        res.apiResponse(items);
+        return res.apiResponse(items);
     });
 }
 
@@ -18,3 +19,15 @@ exports.getAttractionById = function (req, res) {
         return res.apiResponse(item);
     });
 }
+
+/** * Get Attraction by City */
+exports.getAttractionByCity = function (req, res) {
+    var cityId = req.params.id;
+    City.model
+        .findById(cityId).populate('attration')
+        .exec(function (err, item) {
+            if (err) return res.apiError('database error', err);
+            if (!item) return res.apiError('not found');
+            return res.apiResponse(item.attration);
+        });
+};
