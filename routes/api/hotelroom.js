6a0e5df82529338b9587759e4f97221122a98fb8
David Xia <apolo2david@yahoo.com>
2019-04-12 20:38:59 +1000

    Update hotelroom.js

--- routes/api/hotelroom.js
+++ routes/api/hotelroom.js
@@ -1,20 +1,33 @@
 // API to get HotelRoom
-var keystone = require('keystone'),
-    HotelRoom = keystone.list('HotelRoom');
+var keystone = require('keystone');
+var HotelRoom = keystone.list('HotelRoom');
+var Hotel = keystone.list('Hotel');
 
 /** * Get List of HotelRoom */
-exports.getHotelRoom = function (req, res) {
+exports.getAllHotelRoom = function (req, res) {
     HotelRoom.model.find(function (err, items) {
         if (err) return res.apiError('database error', err);
-        res.apiResponse(items);
+        return res.apiResponse(items);
     });
-}
+};
 
 /** * Get HotelRoom by ID */
 exports.getHotelRoomById = function (req, res) {
     HotelRoom.model.findById(req.params.id).exec(function (err, item) {
         if (err) return res.apiError('database error', err);
         if (!item) return res.apiError('not found');
-        res.apiResponse(item);
+        return res.apiResponse(item);
     });
-}
+};
+
+/** * Get HotelRoom by Hotel */
+exports.getHotelRoomByHotel = function (req, res) {
+    var hotelId = req.params.id;
+    Hotel.model
+        .findById(hotelId).populate('hotelroom')
+        .exec(function (err, item) {
+            if (err) return res.apiError('database error', err);
+            if (!item) return res.apiError('not found');
+            return res.apiResponse(item.hotelroom);
+        });
+};
