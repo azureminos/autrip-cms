999d1dc68f641d024eab7e42199871b1f6d95709
David Xia <apolo2david@yahoo.com>
2019-05-10 14:47:08 +1000

    Update travelpackage.js

--- routes/api/travelpackage.js
+++ routes/api/travelpackage.js
@@ -1,7 +1,6 @@
 // API to get TravelPackage
 var keystone = require('keystone');
 var TravelPackage = keystone.list('TravelPackage');
-var Country = keystone.list('Country');
 
 /** * Get List of TravelPackage */
 exports.getAllTravelPackage = function (req, res) {
@@ -13,10 +12,11 @@ exports.getAllTravelPackage = function (req, res) {
 
 /** * Get TravelPackage by ID */
 exports.getTravelPackageById = function (req, res) {
-	TravelPackage.model.findById(req.params.id).exec(function (err, item) {
-		if (err) return res.apiError('database error', err);
-		if (!item) return res.apiError('not found');
-		return res.apiResponse(item);
+	TravelPackage.model.findById(req.params.id)
+		.exec(function (err, item) {
+			if (err) return res.apiError('database error', err);
+			if (!item) return res.apiError('not found');
+			return res.apiResponse(item);
 	});
 };
 
@@ -24,25 +24,9 @@ exports.getTravelPackageById = function (req, res) {
 exports.getTravelPackageByParams = function (req, res) {
 	console.log('>>>>Calling getTravelPackageByParams', req.body);
 	var query = req.body;
-	if (query.country) {
-		if (query.country.id) {
-			Country.model
-				.findById(query.country.id).populate('packages')
-				.exec(function (err, item) {
-					if (err) return res.apiError('database error', err);
-					if (!item) return res.apiError('not found');
-					return res.apiResponse(item.packages);
-				});
-		} else if (query.country.name) {
-			Country.model
-				.findOne({ name: query.country.name }).populate('packages')
-				.exec(function (err, item) {
-					if (err) return res.apiError('database error', err);
-					if (!item) return res.apiError('not found');
-					return res.apiResponse(item.packages);
-				});
-		}
-	} else {
-		return res.apiError('invalid query request', query);
-	}
+	TravelPackage.model.find(query)
+		.exec(function (err, items) {
+			if (err) return res.apiError('database error', err);
+			return res.apiResponse(items);
+	});
 };
