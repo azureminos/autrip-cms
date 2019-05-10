4f05cf516392b4defd9aad7466d35f8165197d7c
David Xia <apolo2david@yahoo.com>
2019-05-10 15:54:19 +1000

    Update travelpackage.js

--- routes/api/travelpackage.js
+++ routes/api/travelpackage.js
@@ -2,11 +2,33 @@
 var keystone = require('keystone');
 var TravelPackage = keystone.list('TravelPackage');
 
+var parseTravelPackage = function (input) {
+	if (Array.isArray(input)) {
+		var rs = [];
+		_.each(input, function (item) {
+			var r = _.pick(item, 'slug', 'name', 'description', 'finePrint', 'notes', 'departureDate',
+				'effectiveTo', 'effectiveFrom', 'isExtention', 'isCustomisable', 'isPromoted', 'state',
+				'maxParticipant', 'totalDays', 'additionalField');
+			r.id = item._id;
+			r.imageUrl = item.image.secure_url;
+			rs.push(r);
+		});
+		return rs;
+	} else {
+		var r = _.pick(item, 'slug', 'name', 'description', 'finePrint', 'notes', 'departureDate',
+			'effectiveTo', 'effectiveFrom', 'isExtention', 'isCustomisable', 'isPromoted', 'state',
+			'maxParticipant', 'totalDays', 'additionalField');
+		r.id = input._id;
+		r.imageUrl = item.image.secure_url;
+		return r;
+	}
+};
+
 /** * Get List of TravelPackage */
 exports.getAllTravelPackage = function (req, res) {
 	TravelPackage.model.find(function (err, items) {
 		if (err) return res.apiError('database error', err);
-		return res.apiResponse(items);
+		return res.apiResponse(parseTravelPackage(items));
 	});
 };
 
@@ -16,7 +38,7 @@ exports.getTravelPackageById = function (req, res) {
 		.exec(function (err, item) {
 			if (err) return res.apiError('database error', err);
 			if (!item) return res.apiError('not found');
-			return res.apiResponse(item);
+			return res.apiResponse(parseTravelPackage(item));
 	});
 };
 
@@ -27,6 +49,6 @@ exports.getTravelPackageByParams = function (req, res) {
 	TravelPackage.model.find(query)
 		.exec(function (err, items) {
 			if (err) return res.apiError('database error', err);
-			return res.apiResponse(items);
+			return res.apiResponse(parseTravelPackage(items);
 	});
 };
