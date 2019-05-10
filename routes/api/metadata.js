75171358257ef43ac28b1ac66c9f1a5e5d573873
David Xia <apolo2david@yahoo.com>
2019-05-10 11:36:46 +1000

    Create metadata.js

---
+++ routes/api/metadata.js
@@ -0,0 +1,62 @@
+// API to get Country
+var _ = require('lodash');
+var async = require('async');
+var keystone = require('keystone');
+
+var Country = keystone.list('Country');
+
+var parseCountry = function (input) {
+	if (Array.isArray(input)) {
+		var rs = [];
+		_.each(input, function (item) {
+			var r = {};
+      r.id = item._id;
+      r.name = item.name;
+			rs.push(r);
+		});
+		return rs;
+	} else {
+    var r = {};
+    r.id = item._id;
+    r.name = item.name;
+		return r;
+	}
+};
+
+/** * Get Metadata */
+exports.getMetadata = function (req, res) {
+  var query = req.body;
+
+  const result = {
+    state: ['draft', 'published', 'archived']
+  };
+
+  const getCountryList = function (callback) {
+    Country
+      .find()
+      .exec(function (err, items) {
+        if (err || !items) return callback();
+        // If yes, bypass; if no, update carRate.package
+        result.country = parseCountry(items);
+        return callback();
+      });
+  };
+
+  const returnMetadata = function() {
+    let rs = {};
+    if (query.keys) {
+      _.each(query.keys, function(item) {
+        rs[item] = result[item];
+      });
+    } else {
+      rs = result;
+    }
+    return res.apiResponse(rs);
+  }
+
+  async.series(
+    [
+      getCountryList,
+    ], returnMetadata);
+
+};
