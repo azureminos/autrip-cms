8282bf25639d6d036832b479924b831194344967
apolo2david@yahoo.com <apolo2david@yahoo.com>
2019-05-13 01:07:23 +1000

    fixed NextJS version

--- pages/index.js
+++ pages/index.js
@@ -1,23 +1,79 @@
-import { Component } from 'react';
-import axios from 'axios';
+/* eslint-disable jsx-a11y/anchor-is-valid */
 
-class App extends Component {
+import React from 'react';
+import PropTypes from 'prop-types';
+import Button from '@material-ui/core/Button';
+import Dialog from '@material-ui/core/Dialog';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogActions from '@material-ui/core/DialogActions';
+import Typography from '@material-ui/core/Typography';
+import { withStyles } from '@material-ui/core/styles';
+import Link from 'next/link';
 
-	static async getInitialProps () {
-		const urlGetPackages = `${process.env.API_BASE_URL}/api/travelpackage`;
-		let resPackages = await axios.get(urlGetPackages);
+const styles = theme => ({
+  root: {
+    textAlign: 'center',
+    paddingTop: theme.spacing.unit * 20,
+  },
+});
 
-		const urlGetMetadata = `${process.env.API_BASE_URL}/api/metadata`;
-		let resMetadata = await axios.get(urlGetMetadata);
+class Index extends React.Component {
+  state = {
+    open: false,
+  };
 
-		return { packages: resPackages.data, filters: resMetadata.data };
-	}
+  handleClose = () => {
+    this.setState({
+      open: false,
+    });
+  };
 
-	render () {
-		return (
-			<div>Hello World</div>
-		);
-	};
+  handleClick = () => {
+    this.setState({
+      open: true,
+    });
+  };
+
+  render() {
+    const { classes } = this.props;
+    const { open } = this.state;
+
+    return (
+      <div className={classes.root}>
+        <Dialog open={open} onClose={this.handleClose}>
+          <DialogTitle>Super Secret Password</DialogTitle>
+          <DialogContent>
+            <DialogContentText>1-2-3-4-5</DialogContentText>
+          </DialogContent>
+          <DialogActions>
+            <Button color="primary" onClick={this.handleClose}>
+              OK
+            </Button>
+          </DialogActions>
+        </Dialog>
+        <Typography variant="h4" gutterBottom>
+          Material-UI
+        </Typography>
+        <Typography variant="subtitle1" gutterBottom>
+          example project
+        </Typography>
+        <Typography gutterBottom>
+          <Link href="/about">
+            <a>Go to the about page</a>
+          </Link>
+        </Typography>
+        <Button variant="contained" color="secondary" onClick={this.handleClick}>
+          Super Secret Password
+        </Button>
+      </div>
+    );
+  }
 }
 
-export default App;
+Index.propTypes = {
+  classes: PropTypes.object.isRequired,
+};
+
+export default withStyles(styles)(Index);
