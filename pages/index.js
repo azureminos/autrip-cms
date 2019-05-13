75c11eb86475bb08f1fc1eff58408babb7e67769
apolo2david@yahoo.com <apolo2david@yahoo.com>
2019-05-13 23:51:13 +1000

    up

--- pages/index.js
+++ pages/index.js
@@ -1,79 +1,51 @@
-/* eslint-disable jsx-a11y/anchor-is-valid */
+import { Component } from 'react';
+import _ from 'lodash';
+import axios from 'axios';
+import { Paper, Typography } from '@material-ui/core';
+import FullWidthTabs from '../components/fixed-tab';
 
-import React from 'react';
-import PropTypes from 'prop-types';
-import Button from '@material-ui/core/Button';
-import Dialog from '@material-ui/core/Dialog';
-import DialogTitle from '@material-ui/core/DialogTitle';
-import DialogContent from '@material-ui/core/DialogContent';
-import DialogContentText from '@material-ui/core/DialogContentText';
-import DialogActions from '@material-ui/core/DialogActions';
-import Typography from '@material-ui/core/Typography';
-import { withStyles } from '@material-ui/core/styles';
-import Link from 'next/link';
+class App extends Component {
 
-const styles = theme => ({
-  root: {
-    textAlign: 'center',
-    paddingTop: theme.spacing.unit * 20,
-  },
-});
+	static async getInitialProps () {
+		const urlGetPackages = `${process.env.API_BASE_URL}/api/travelpackage`;
+		let resPackages = await axios.get(urlGetPackages);
 
-class Index extends React.Component {
-  state = {
-    open: false,
-  };
+		const urlGetMetadata = `${process.env.API_BASE_URL}/api/metadata`;
+		let resMetadata = await axios.get(urlGetMetadata);
 
-  handleClose = () => {
-    this.setState({
-      open: false,
-    });
-  };
+		return { packages: resPackages.data, filters: resMetadata.data };
+	}
 
-  handleClick = () => {
-    this.setState({
-      open: true,
-    });
-  };
+	render () {
+		const { packages, filters } = this.props;
+		let tabs, page;
+		//Init tab content to display all packages
+		const tabPackages = (
+			<Typography>
+				Show all packages
+			</Typography>
+		);
+		//Init tab content to display all attractions
+		const tabAttractions = (
+			<Typography>
+				Show all attractions
+			</Typography>
+		);
+		tabs = {
+			Packages: tabPackages,
+			Attractions: tabAttractions,
+		};
+		//Init page
+		page = (
+			<Paper>
+				<FullWidthTabs
+					tabs={tabs}
+				/>
+			</Paper>
+		);
 
-  render() {
-    const { classes } = this.props;
-    const { open } = this.state;
-
-    return (
-      <div className={classes.root}>
-        <Dialog open={open} onClose={this.handleClose}>
-          <DialogTitle>Super Secret Password</DialogTitle>
-          <DialogContent>
-            <DialogContentText>1-2-3-4-5</DialogContentText>
-          </DialogContent>
-          <DialogActions>
-            <Button color="primary" onClick={this.handleClose}>
-              OK
-            </Button>
-          </DialogActions>
-        </Dialog>
-        <Typography variant="h4" gutterBottom>
-          Material-UI
-        </Typography>
-        <Typography variant="subtitle1" gutterBottom>
-          example project
-        </Typography>
-        <Typography gutterBottom>
-          <Link href="/about">
-            <a>Go to the about page</a>
-          </Link>
-        </Typography>
-        <Button variant="contained" color="secondary" onClick={this.handleClick}>
-          Super Secret Password
-        </Button>
-      </div>
-    );
-  }
+		return (page);
+	};
 }
 
-Index.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(Index);
+export default App;
