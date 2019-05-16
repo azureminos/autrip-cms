921877d2b8d0c4d6e316df09dd2823576b55206d
David Xia <apolo2david@yahoo.com>
2019-05-16 17:04:12 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,10 +1,15 @@
 const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 
+const hostname = process.env.API_BASE_URL || 'http://localhost';
+const {DEMO, PORT, LOCAL} = process.env;
+const socketAddress = (DEMO && LOCAL) ? `http://${hostname}:${PORT}` : `wss://${hostname}`;
+
+console.log('>>>>Print env[socketAddress]', socketAddress);
+
 const nextConfig = {
   publicRuntimeConfig: {
-    SOCKET_BASE_URL: process.env.API_BASE_URL || 'http://localhost',
-    SOCKET_PORT: Number(process.env.PORT || '4000'),
+    SOCKET_URL: socketAddress,
   },
 };
 
