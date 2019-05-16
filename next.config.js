d1167ddd8411632aa802402878153f23cacc5b98
David Xia <apolo2david@yahoo.com>
2019-05-16 15:07:21 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -1,11 +1,13 @@
 const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 
+const nextConfig = {
+  SOCKET_BASE_URL : "test next",
+};
+
 module.exports = withPlugins(
   [
     withCSS,
   ],
-  {
-    SOCKET_BASE_URL : "test next",
-  },
+  nextConfig
 );
