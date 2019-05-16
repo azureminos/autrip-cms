2ef68b2df5b3ee49ffcf0d97110650a368001f4d
David Xia <apolo2david@yahoo.com>
2019-05-16 15:26:48 +1000

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@ const withCSS = require('@zeit/next-css');
 const withPlugins = require('next-compose-plugins');
 
 const nextConfig = {
-  env: {SOCKET_BASE_URL : "test next"},
+  publicRuntimeConfig: {SOCKET_BASE_URL : "test next"},
 };
 
 module.exports = withPlugins(
