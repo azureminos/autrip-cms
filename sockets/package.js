0a37e68908d39153e890895c27334fa246e836c8
David Xia <apolo2david@yahoo.com>
2019-05-17 16:06:15 +1000

    Update package.js

--- sockets/package.js
+++ sockets/package.js
@@ -2,7 +2,7 @@ const async = require('async');
 const keystone = require('keystone');
 const helper = require('../lib/object-parser');
 
-exports.getPackageDetails = ({request: {id}, sendStatus, socket}) => {
+const getPackageDetails = ({request: {id}, sendStatus, socket}) => {
   console.log('>>>>server socket received event[push:package:get]', id);
   // async calls
   async.parallel({
@@ -19,8 +19,8 @@ exports.getPackageDetails = ({request: {id}, sendStatus, socket}) => {
     console.log('>>>>server final callback for event[push:package:get]', results);
     socket.emit('package:get', results);
   });
+};
 
-  channel('disconnect', () => {
-    console.log('>>>>User disconnected');
-  });
+export default {
+  getPackageDetails,
 };
