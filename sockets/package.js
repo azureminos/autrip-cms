961d7ca47e66f38f5f4314517d8d37fb151228f2
David Xia <apolo2david@yahoo.com>
2019-05-17 16:43:53 +1000

    Update package.js

--- sockets/package.js
+++ sockets/package.js
@@ -2,18 +2,58 @@ const async = require('async');
 const keystone = require('keystone');
 const helper = require('../lib/object-parser');
 
+const TravelPackage = keystone.list('TravelPackage');
+const TravelPackage = keystone.list('TravelPackage');
+const TravelPackage = keystone.list('TravelPackage');
+const TravelPackage = keystone.list('TravelPackage');
+const TravelPackage = keystone.list('TravelPackage');
+
 exports.getPackageDetails = ({request: {id}, sendStatus, socket}) => {
   console.log('>>>>server socket received event[push:package:get]', id);
   // async calls
   async.parallel({
     package: (callback) => {
-      const TravelPackage = keystone.list('TravelPackage');
       TravelPackage.model
         .findById(id)
         .exec(function (err, item) {
-          console.log('>>>>server async calls for event[push:package:get]', item);
+          //console.log('>>>>server async calls for event[push:package:get]', item);
           return callback(null, helper.parseTravelPackage(item));
         });
+    },
+    packageItems: (callback) => {
+      TravelPackage.model
+      .findById(query.package.id).populate('packageItems')
+      .exec(function (err, item) {
+        return callback(null, item.packageItems);
+      });
+    },
+    packageHotels: (callback) => {
+      TravelPackage.model
+      .findById(query.package.id).populate('packageHotels')
+      .exec(function (err, item) {
+        return callback(null, item.packageHotels);
+      });
+    },
+    packageRates: (callback) => {
+      TravelPackage.model
+      .findById(query.package.id).populate('packageRates')
+      .exec(function (err, item) {
+        return callback(null, item.packageRates);
+      });
+    },
+    carRates: (callback) => {
+      TravelPackage.model
+      .findById(query.package.id).populate('carRates')
+      .exec(function (err, item) {
+        return callback(null, item.carRates);
+      });
+    },
+    flightRates: (callback) => {
+      TravelPackage.model
+      .findById(query.package.id).populate('flightRates')
+      .exec(function (err, item) {
+        return callback(null, item.flightRates);
+      });
     },
   }, function (err, results) {
     console.log('>>>>server final callback for event[push:package:get]', results);
