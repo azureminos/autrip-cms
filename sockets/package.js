858f046330876ad0de7011f34f80881d6f6cb369
David Xia <apolo2david@yahoo.com>
2019-05-17 15:24:56 +1000

    Create package.js

---
+++ sockets/package.js
@@ -0,0 +1,30 @@
+const async = require('async');
+const keystone = require('keystone');
+const helper = require('../lib/object-parser');
+
+const getPackageDetails = ({request: {id}, sendStatus, socket}) => {
+  console.log('>>>>server socket received event[push:package:get]', id);
+  // async calls
+  async.parallel({
+    package: (callback) => {
+      const TravelPackage = keystone.list('TravelPackage');
+      TravelPackage.model
+        .findById(id)
+        .exec(function (err, item) {
+          console.log('>>>>server async calls for event[push:package:get]', item);
+          return callback(null, helper.parseTravelPackage(item);
+        });
+    },
+  }, function (err, results) {
+    console.log('>>>>server final callback for event[push:package:get]', results);
+    socket.emit('package:get', results);
+  });
+
+  channel('disconnect', () => {
+    console.log('>>>>User disconnected');
+  });
+};
+
+export default {
+  getPackageDetails,
+};
