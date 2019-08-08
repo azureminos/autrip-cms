e3fe617db58e671a6551daea6f6b85ec9ad4cc96
David Xia <apolo2david@yahoo.com>
2019-08-08 14:21:32 +1000

    Update package.js

--- sockets/package.js
+++ sockets/package.js
@@ -176,3 +176,84 @@ exports.getFilteredPackages = ({ request, sendStatus, socket }) => {
 		});
 	});
 };
+
+exports.publishPackage = ({
+	request: { id, status },
+	sendStatus,
+	socket,
+}) => {
+	// console.log('>>>>server socket received event[push:package:status]', id);
+	const TravelPackage = keystone.list('TravelPackage');
+
+	TravelPackage.model
+		.update({ _id: id }, { state: status })
+		.exec(function (err, res) {
+			// console.log(`>>>>TravelPackage.update[${id}: ${status}]`, res);
+			if (isRefreshAll) {
+				TravelPackage.model.find(function (err, items) {
+					socket.emit('package:refreshAll', {
+						packages: helper.parseTravelPackage(items),
+					});
+				});
+			} else {
+				async.parallel(
+					{
+						packageSummary: callback => {
+							TravelPackage.model.findById(id).exec(function (err, item) {
+								// console.log('>>>>server async calls for event[push:package:get]', item);
+								return callback(null, helper.parseTravelPackage(item));
+							});
+						},
+						packageItems: callback => {
+							TravelPackage.model
+								.findById(id)
+								.populate('packageItems')
+								.exec(function (err, item) {
+									return callback(null, item.packageItems);
+								});
+						},
+						packageHotels: callback => {
+							TravelPackage.model
+								.findById(id)
+								.populate('packageHotels')
+								.exec(function (err, item) {
+									return callback(null, item.packageHotels);
+								});
+						},
+						packageRates: callback => {
+							TravelPackage.model
+								.findById(id)
+								.populate('packageRates')
+								.exec(function (err, item) {
+									return callback(null, item.packageRates);
+								});
+						},
+						carRates: callback => {
+							// TBD
+							callback(null, []);
+						},
+						flightRates: callback => {
+							TravelPackage.model
+								.findById(id)
+								.populate('flightRates')
+								.exec(function (err, item) {
+									return callback(null, item.flightRates);
+								});
+						},
+					},
+					function (err, results) {
+						// console.log('>>>>server final callback for event[push:package:get]', results);
+						socket.emit('package:get', results);
+					}
+				);
+			}
+		});
+};
+
+exports.archivePackage = ({
+	request: { id, status },
+	sendStatus,
+	socket,
+}) => {
+	// console.log('>>>>server socket received event[push:package:status]', id);
+};
